d89d34b0799a3dd538c9f43371b5e448ccfc8d5c
arash-gmc <arash.green.423@gmail.com>
2023-10-29 15:41:29 +0330

    improve issue details skeleton

--- app/issue/[id]/loading.tsx
+++ app/issue/[id]/loading.tsx
@@ -1,20 +1,20 @@
-import { Heading, Flex, Card } from "@radix-ui/themes";
+import { Heading, Flex, Card, Box } from "@radix-ui/themes";
 import { Skeleton } from "@/components";
 
 const loading = () => {
   return (
-    <div>
+    <Box className="px-3 max-w-2xl">
       <Heading>
         <Skeleton width="15rem" />
       </Heading>
       <Flex className="space-x-3 my-3">
-        <Skeleton width="5rem" />
-        <Skeleton width="7rem" />
+        <Skeleton width="3rem" />
+        <Skeleton width="11rem" />
       </Flex>
-      <Card>
-        <Skeleton count={3} />
-      </Card>
-    </div>
+      <Box className="border-2 p-4 rounded-xl">
+        <Skeleton height="10rem" />
+      </Box>
+    </Box>
   );
 };
 
