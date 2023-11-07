207b02c9dc5eab7ba93f72b597460e856ec01315
arash-gmc <arash.green.423@gmail.com>
2023-11-07 09:55:11 +0330

    add statuses to status filter

--- app/issue/StatusFilter.tsx
+++ app/issue/StatusFilter.tsx
@@ -1,12 +1,34 @@
+import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import React from "react";
+import { useRouter } from "next/navigation";
+
+const statuses: { label: string; value: Status | "0" }[] = [
+  { label: "All Status", value: "0" },
+  { label: "Open", value: "OPEN" },
+  { label: "Closed", value: "CLOSED" },
+  { label: "in Progress", value: "IN_PROGRESS" },
+];
 
 const StatusFilter = () => {
+  const router = useRouter();
   return (
-    <Select.Root>
+    <Select.Root
+      onValueChange={(selected) => {
+        const query = selected === "0" ? "" : "?status=" + selected;
+        router.push("/issue" + query);
+      }}
+    >
       <Select.Trigger placeholder="Filter By Status" />
       <Select.Content>
-        <Select.Item value="mine">My Status</Select.Item>
+        {statuses.map((status) => (
+          <Select.Item
+            key={status.value}
+            value={status.value}
+          >
+            {status.label}
+          </Select.Item>
+        ))}
       </Select.Content>
     </Select.Root>
   );
