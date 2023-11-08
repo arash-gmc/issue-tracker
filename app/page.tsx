25bd679a2ad9133ad65f758d9f307b29b067b119
arash-gmc <arash.green.423@gmail.com>
2023-11-08 14:04:42 +0330

    layout the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
 import prisma from "@/prisma/client";
 import IssueSummery from "./IssueSummery";
 import IssueCharts from "./IssueCharts";
+import { Flex, Grid } from "@radix-ui/themes";
+import LastIssues from "./LastIssues";
 
 export default async function Home() {
-  return <IssueCharts />;
+  return (
+    <Grid
+      columns={{ initial: "1", sm: "2" }}
+      gap="3"
+    >
+      <Flex
+        direction="column"
+        gap="3"
+      >
+        <IssueSummery />
+        <IssueCharts />
+      </Flex>
+      <LastIssues />
+    </Grid>
+  );
 }
