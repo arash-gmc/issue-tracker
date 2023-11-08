94b46e3be4ce5c1b1ad633a9961bd729cbfd9b6e
arash-gmc <arash.green.423@gmail.com>
2023-11-08 06:45:43 +0330

    add pagination component ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
+import Pagination from "@/components/Pagination";
 
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <Pagination
+      pageSize={10}
+      currentPage={2}
+      itemCount={100}
+    />
+  );
 }
---
+++ components/Pagination.tsx
@@ -0,0 +1,58 @@
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  DoubleArrowLeftIcon,
+  DoubleArrowRightIcon,
+} from "@radix-ui/react-icons";
+import { Button, Flex, Text } from "@radix-ui/themes";
+import React from "react";
+
+interface Props {
+  currentPage: number;
+  itemCount: number;
+  pageSize: number;
+}
+
+const Pagination = ({ currentPage, itemCount, pageSize }: Props) => {
+  const pageCount = Math.ceil(itemCount / pageSize);
+  return (
+    <Flex
+      align="center"
+      gap="2"
+    >
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+      >
+        <DoubleArrowLeftIcon />
+      </Button>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+      >
+        <ChevronLeftIcon />
+      </Button>
+      <Text>
+        Page {currentPage} of {pageCount}
+      </Text>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === pageCount}
+      >
+        <ChevronRightIcon />
+      </Button>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === pageCount}
+      >
+        <DoubleArrowRightIcon />
+      </Button>
+    </Flex>
+  );
+};
+
+export default Pagination;
